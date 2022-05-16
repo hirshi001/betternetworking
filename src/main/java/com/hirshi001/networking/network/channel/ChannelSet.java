d03191c7bbfdb48dc300cbd4c34e0463cdc2119d
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2022-05-15 21:06:44 -0700

    changed ChannelSet syncronicity and added new helper method

--- src/main/java/com/hirshi001/networking/network/channel/ChannelSet.java
+++ src/main/java/com/hirshi001/networking/network/channel/ChannelSet.java
@@ -6,15 +6,18 @@ import com.hirshi001.networking.packetregistry.PacketRegistry;
 import com.hirshi001.restapi.RestFuture;
 
 import java.util.*;
+import java.util.function.Supplier;
 
 public class ChannelSet implements Set<Channel> {
 
     private final Server server;
     private final Set<Channel> channels;
+    private final Object lock;
 
     public ChannelSet(Server server) {
         this.server = server;
-        channels = Collections.synchronizedSet(new LinkedHashSet<>());
+        channels = new LinkedHashSet<>();
+        lock = new Object();
     }
 
     public Server getServer() {
@@ -23,14 +26,18 @@ public class ChannelSet implements Set<Channel> {
 
     public RestFuture<?, ChannelSet> sendTCPToAll(Packet packet, PacketRegistry packetRegistry) {
         return RestFuture.create(()->{
-            channels.forEach(channel -> channel.sendTCP(packet, packetRegistry));
+            synchronized (lock) {
+                channels.forEach(channel -> channel.sendTCP(packet, packetRegistry));
+            }
             return this;
         });
     }
 
     public RestFuture<?, ChannelSet> sendUDPToAll(Packet packet, PacketRegistry packetRegistry) {
         return RestFuture.create(()->{
-            channels.forEach(channel -> channel.sendUDP(packet, packetRegistry));
+            synchronized (lock) {
+                channels.forEach(channel -> channel.sendUDP(packet, packetRegistry));
+            }
             return this;
         });
     }
@@ -47,7 +54,22 @@ public class ChannelSet implements Set<Channel> {
 
     @Override
     public boolean contains(Object o) {
-        return channels.contains(o);
+        synchronized (lock) {
+            return channels.contains(o);
+        }
+    }
+
+    public Channel getOrCreate(byte[] address, int port, Supplier<Channel> channelSupplier) {
+        synchronized (lock) {
+            for (Channel channel : channels) {
+                if (Arrays.equals(channel.getAddress(), address) && channel.getPort() == port) {
+                    return channel;
+                }
+            }
+            Channel channel = channelSupplier.get();
+            add(channel);
+            return channel;
+        }
     }
 
     @Override
@@ -57,46 +79,64 @@ public class ChannelSet implements Set<Channel> {
 
     @Override
     public Object[] toArray() {
-        return channels.toArray();
+        synchronized (lock) {
+            return channels.toArray();
+        }
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return channels.toArray(a);
+        synchronized (lock) {
+            return channels.toArray(a);
+        }
     }
 
     @Override
     public boolean add(Channel channel) {
-        return channels.add(channel);
+        synchronized (lock) {
+            return channels.add(channel);
+        }
     }
 
     @Override
     public boolean remove(Object o) {
-        return channels.remove(o);
+        synchronized (lock) {
+            return channels.remove(o);
+        }
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return channels.containsAll(c);
+        synchronized (lock) {
+            return channels.containsAll(c);
+        }
     }
 
     @Override
     public boolean addAll(Collection<? extends Channel> c) {
-        return channels.addAll(c);
+        synchronized (lock) {
+            return channels.addAll(c);
+        }
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return channels.removeAll(c);
+        synchronized (lock) {
+            return channels.removeAll(c);
+        }
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return channels.retainAll(c);
+        synchronized (lock) {
+            return channels.retainAll(c);
+        }
     }
 
     @Override
     public void clear() {
-        channels.clear();
+        synchronized (lock) {
+            channels.clear();
+        }
     }
 }
