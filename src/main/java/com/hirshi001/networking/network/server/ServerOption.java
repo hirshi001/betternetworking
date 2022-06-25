612d5d2b460ed2165ac094d9f682b7ea4693b8b5
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2022-06-25 14:12:03 -0700

    update bb

--- src/main/java/com/hirshi001/networking/network/server/ServerOption.java
+++ src/main/java/com/hirshi001/networking/network/server/ServerOption.java
@@ -7,9 +7,9 @@ import java.net.ServerSocket;
 public class ServerOption<T> extends Option<T> {
 
     public static final ServerOption<Integer> MAX_CLIENTS = new ServerOption<>("max_clients", Integer.class);
-    public static final ServerOption<Boolean> REUSE_ADDRESS = new ServerOption<>("reuse_address", Boolean.class);
     public static final ServerOption<Integer> RECEIVE_BUFFER_SIZE = new ServerOption<>("receive_buffer_size", Integer.class);
-    public static final ServerOption<Integer> SET_SO_TIMEOUT = new ServerOption<>("set_so_timeout", Integer.class);
+    public static final ServerOption<Integer> MAX_PAYLOAD_SIZE = new ServerOption<>("max_payload_size", Integer.class);
+    public static final ServerOption<Integer> MAX_PACKET_SIZE = new ServerOption<>("max_packet_size", Integer.class);
 
     public ServerOption(String name, Class<T> type) {
         super(name, type);
