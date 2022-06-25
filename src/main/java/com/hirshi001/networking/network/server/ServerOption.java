9e0e1fc2cb3046061df50276788fc72049f93d10
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2022-06-25 15:01:01 -0700

    fixed ServerOption

--- src/main/java/com/hirshi001/networking/network/server/ServerOption.java
+++ src/main/java/com/hirshi001/networking/network/server/ServerOption.java
@@ -3,7 +3,7 @@ package com.hirshi001.networking.network.server;
 public class ServerOption<T> extends com.hirshi001.networking.util.Option<T> {
 
     public static final ServerOption<Integer> MAX_CLIENTS = new ServerOption<>("max_clients", Integer.class);
-    public static final NetworkSideOption<Integer> RECEIVE_BUFFER_SIZE = new NetworkSideOption<>("receive_buffer_size", Integer.class); //only for udp packets
+    public static final ServerOption<Integer> RECEIVE_BUFFER_SIZE = new ServerOption<>("receive_buffer_size", Integer.class); //only for udp packets
 
 
     public ServerOption(String name, Class<T> type) {
